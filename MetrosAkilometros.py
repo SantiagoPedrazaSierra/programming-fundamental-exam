e2a5d5bdfb57fe6e881f1e49d475b9d0782921b2
SantiagoPedrazaSierra <186082296+SantiagoPedrazaSierra@users.noreply.github.com>
2024-11-05 12:43:07 -0500

    Ejercicios completos

---
+++ MetrosAkilometros.py
@@ -0,0 +1,8 @@
+#Solicitar metros
+metros=float(input("Metros:"))
+
+#Operacion para pasar de metros a kilometros 
+kilometros= metros/1000
+
+#Mostrar distancia en kilometros 
+print(f"{round(metros)}m = {round(kilometros)}km.")
