325afeae141affe5a4f7357282b6b13ced196c77
SantiagoPedrazaSierra <186082296+SantiagoPedrazaSierra@users.noreply.github.com>
2024-11-05 12:55:23 -0500

    Ejercicios completos

--- MetrosAkilometros.py
+++
@@ -1,8 +0,0 @@
-#Solicitar metros
-metros=float(input("Metros:"))
-
-#Operacion para pasar de metros a kilometros 
-kilometros= metros/1000
-
-#Mostrar distancia en kilometros 
-print(f"{round(metros)}m = {round(kilometros)}km.")
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#PRIMER EJERCICIO
+
 #Pedir a usuario ingrese voltajes 
 print("Ingrese 5 voltajes para obtener su promedio.")
 print()
@@ -18,4 +20,50 @@ if prom_voltaje >220:
 else:
     print("VOLTAJE CORRECTO")
 
-   
+
+#SEGUNDO EJERCICIO
+
+#Solicitar valor de lados a usuario 
+lado=float(input("Ingrese valor de lado:"))
+
+#Operacion para hallar area segun el lado 
+area=1.732/4 * lado**2
+
+#Mostrar resultado de area 
+if area > 1000:
+    print("DATOS NO VALIDOS")
+else:
+    print(area)
+
+
+#TERCEER EJERCICIO
+
+#Solicitar a usuario voltajes 
+voltaje_1=float(input("Voltaje 1: "))
+voltaje_2=float(input("Voltaje 2: "))
+voltaje_3=float(input("Voltaje 3: "))
+
+#Operacion promedio 
+promedio=(voltaje_1+voltaje_2+voltaje_3) / 3
+
+#Comprobar valor de voltajes si es mayor o menor a 115
+if promedio< 115:
+    print("VOLTAJE CORRECTO")
+
+elif promedio > 115 and promedio < 220:
+    print("ALTO VOLTAJE")
+
+elif promedio > 220:
+        print("PELIGRO")
+
+
+#CUARTO EJERCICIO
+
+#Solicitar metros
+metros=float(input("Metros:"))
+
+#Operacion para pasar de metros a kilometros 
+kilometros= metros/1000
+
+#Mostrar distancia en kilometros 
+print(f"{round(metros)}m = {round(kilometros)}km.")
